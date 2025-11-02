af9765075200882df0f5f8e60dcad5dc6c45f85a
nick <atharva.st.05@gmail.com>
2025-11-02 18:53:34 +0530

    creating a website using react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+    <h1>chai aur react | Atharva Chauhan</h1>
+    <p>what i learned in today's class was launching a project with react library
+      and we have edited the files in the src folder and then changed it to a lot 
+      simpler </p>
+    </div>
+  );
+}
+
+export default App;
