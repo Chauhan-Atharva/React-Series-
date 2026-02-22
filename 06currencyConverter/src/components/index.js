f2de0d3a576dd7fbff6db972da52ca112b17331e
nick <atharva.st.05@gmail.com>
2026-02-22 12:50:02 +0530

    Context api to do list

--- 06currencyConverter/src/components/index.js
+++
@@ -1,4 +0,0 @@
-import InputBox from "./InputBox";
-
-export {InputBox}
-//this approach is better for exporting in bigger projects 
---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { useToDo } from "../contexts";
+import { useState } from "react";
+
+function ToDoItem({todo}){
+    const [isToDoEditable, setIsToDoEditable ] = useState(false);
+    const [toDoMsg, setToDoMsg]= useState(todo.todo);
+    const{updateToDo, deleteToDo, toggleComplete } = useToDo();
+
+    const editTodo = () =>{
+        updateToDo(todo.id, {...todo, todo: toDoMsg});
+        setIsToDoEditable(false);
+    }
+
+    const toggleCompleted = () => {
+        toggleComplete(todo.id)
+    }
+ return (
+      <div
+          className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
+              todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
+          }`}
+      >
+          <input
+              type="checkbox"
+              className="cursor-pointer"
+              checked={todo.completed}
+              onChange={toggleCompleted}
+          />
+          <input
+              type="text"
+              className={`border outline-none w-full bg-transparent rounded-lg ${
+                  isToDoEditable ? "border-black/10 px-2" : "border-transparent"
+              } ${todo.completed ? "line-through" : ""}`}
+              value={toDoMsg}
+              onChange={(e) => setToDoMsg(e.target.value)}
+              readOnly={!isToDoEditable}
+          />
+          {/* Edit, Save Button */}
+          <button
+              className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
+              onClick={() => {
+                  if (todo.completed) return;
+
+                  if (isToDoEditable) {
+                      editTodo();
+                  } else setIsToDoEditable((prev) => !prev);
+              }}
+              disabled={todo.completed}
+          >
+              {isToDoEditable ? "📁" : "✏️"}
+          </button>
+          {/* Delete Todo Button */}
+          <button
+              className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
+              onClick={() => deleteToDo(todo.id)}
+          >
+              ❌
+          </button>
+      </div>
+  );
+}
+
+export default ToDoItem;
